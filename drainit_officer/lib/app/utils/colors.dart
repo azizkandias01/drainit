2eef463a2b485fae3146a0e75f7fcba0b9978942
zeekands <aziz18ti@mahasiswa.pcr.ac.id>
2022-03-05 15:20:21 +0700

    delete all files

--- drainit_officer/lib/app/utils/colors.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-final kMainGreen = Color(0xFF1CC37A);
-final kSecondaryGreen = Color(0xFF78E4A9);
--- drainit_public/android/app/src/main/kotlin/com/zeekands/drainit_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.zeekands.drainit_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
